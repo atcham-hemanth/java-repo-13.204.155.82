3d22923316cfa769211f4e19126baa0b1e5fd7d6
atcham-hemanth <ubuntu@ip-172-31-17-254.ap-south-1.compute.internal>
2026-05-18 11:25:01 +0000

    Testing webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello welcome to hemanths , Jenkins CI/CD Pipeline!");
+        System.out.println("Hello welcome to hemanths , Jenkins CI/CD Pipelinew which is new!");
     }
 }
 
